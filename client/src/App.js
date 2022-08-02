be97747d025e9adb58663047c01e101f86248b42
Luiz Henrique <luiz543321@gmail.com>
2022-08-02 00:58:58 -0300

    Routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
-import SignUp from './Components/SignUp/SignUp';
-import SignInSide from './Components/SingIn/SignInSide';
+// import logo from './logo.svg';
+// import './App.css';
 
+// import {
+//   BrowserRouter as Router,
+//   Route,
+//   Routes,
+//   Link,
+// } from 'react-router-dom';
 
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Link,
-} from 'react-router-dom';
+// function App() {
+//   return (
+//     <SignInSide />
+//   );
+// }
 
-function App() {
+// export default App;
+
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import SignUp from "./Components/SignUp/SignUp";
+import SignInSide from "./Components/SingIn/SignInSide";
+
+export default function BasicExample() {
   return (
-    <SignInSide />
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<SignInSide />} />
+          <Route path="/signin" element={<SignUp />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+// function Home() {
+//   return (
+//     <div>
+//       <h2>Home</h2>
+//       <Link to="/about">About</Link>
+//     </div>
+//   );
+// }
+
+// function About() {
+//   return (
+//     <div>
+//       <h2>About</h2>
+//       <Link to="/dashboard">dashboard</Link>
+//     </div>
+//   );
+// }
+
+// function Dashboard() {
+//   return (
+//     <div>
+//       <h2>Dashboard</h2>
+//       <Link to="/">home</Link>
+//     </div>
+//   );
+// }
