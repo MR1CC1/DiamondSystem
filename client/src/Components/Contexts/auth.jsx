d9e78b66f55b55482d2459b5ed4d217af6cc6940
Luiz Henrique <luiz543321@gmail.com>
2022-09-05 01:51:28 -0300

    Fix login

--- client/src/Components/Contexts/auth.jsx
+++ client/src/Components/Contexts/auth.jsx
@@ -1,66 +1,52 @@
-import React, { createContext, useState, useContext, useEffect } from 'react'
-import { useNavigate } from 'react-router-dom';
-import AppRoutes from '../../AppRoutes';
+import React, { createContext, useState, useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import AppRoutes from "../../AppRoutes";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Alert } from '@mui/material';
-import { api, createSession } from "../services/api.js"
+import { Alert } from "@mui/material";
+import { api, createSession } from "../services/api.js";
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const [user, setUser] = useState(null);
-    const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
+  useEffect(() => {
+    const recoveredUser = localStorage.getItem("user");
+    const token = localStorage.getItem("token");
+    if (recoveredUser && token) {
+      setUser(JSON.parse(recoveredUser));
+      api.defaults.headers.Authorization = `Bearer ${token}`;
+    }
+    setLoading(false);
+  }, []);
 
-    useEffect(() => {
-        const recoveredUser = localStorage.getItem('user');
-        const token = localStorage.getItem("token");
-        if (recoveredUser && token) {
-            setUser(JSON.parse(recoveredUser));
-            api.defaults.headers.Authorization = `Bearer ${token}`;
-        }
-        setLoading(false);
-    }, []);
+  const login = async (token, user) => {
+    localStorage.setItem("user", JSON.stringify(user));
+    localStorage.setItem("token", JSON.stringify(token));
 
+    api.defaults.headers.Authorization = `Bearer ${token}`;
 
+    setUser(user);
+    navigate("/");
+  };
 
-    const login = async (email, password) => {
+  const logout = () => {
+    console.log("logout");
+    localStorage.removeItem("user");
+    localStorage.removeItem("token");
+    api.defaults.headers.Authorization = null;
+    setUser(null);
+    navigate("/singin");
+  };
 
-        const response = await createSession(email, password);
-        console.log('login auth', { email, password });
-
-        const loggedUser = response.data.user;
-        const token = response.data.token;
-
-        // Api criar uma session
-
-
-        localStorage.setItem("user", JSON.stringify(loggedUser));
-        localStorage.setItem("token", JSON.stringify(token));
-
-        api.defaults.headers.Authorization = `Bearer ${token}`;
-
-        setUser(loggedUser);
-        navigate("/");
-
-    };
-
-    const logout = () => {
-        console.log('logout');
-        localStorage.removeItem("user");
-        localStorage.removeItem("token");
-        api.defaults.headers.Authorization = null;
-        setUser(null);
-        navigate("/singin");
-    };
-
-    return (
-        <AuthContext.Provider
-            value={{ authenticated: !!user, user, loading, login, logout }}>
-            {children}
-        </AuthContext.Provider>
-    )
-
-}
+  return (
+    <AuthContext.Provider
+      value={{ authenticated: !!user, user, loading, login, logout }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
