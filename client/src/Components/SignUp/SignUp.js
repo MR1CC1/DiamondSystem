71fa4745a3fc8346d984b83d99c43305cccb1d98
Matheus Ricci <57235913+MR1CC1@users.noreply.github.com>
2022-09-05 02:01:39 -0300

    warnings fix

--- client/src/Components/SignUp/SignUp.js
+++ client/src/Components/SignUp/SignUp.js
@@ -1,20 +1,13 @@
 import * as React from "react";
-import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
 import Link from "@mui/material/Link";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
-import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
-import { createStyles, createTheme, ThemeProvider } from "@mui/material/styles";
-import { green } from "@mui/material/colors";
-import SignInSide from "../SingIn/SignInSide";
-import { padding } from "@mui/system";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import Axios from "axios";
 
 function Copyright(props) {
@@ -73,10 +66,6 @@ export default function SignUp() {
             alignItems: "center",
           }}
         >
-          {/* <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-                        <LockOutlinedIcon />
-                    </Avatar> */}
-
           <Grid item xs={12}>
             <img
               style={{ marginBottom: 30 }}
@@ -137,12 +126,6 @@ export default function SignUp() {
                   autoComplete="new-password"
                 />
               </Grid>
-              {/* <Grid item xs={12}>
-                                <FormControlLabel
-                                    control={<Checkbox value="allowExtraEmails" color="primary" />}
-                                    label="I want to receive inspiration, marketing promotions and updates via email."
-                                />
-                            </Grid> */}
             </Grid>
             <Button
               type="submit"
